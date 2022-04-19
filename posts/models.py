360089b770116061ecb51a2d1f1ec9e82dd955e2
SEUNGYEON YOO <seungyeon323@naver.com>
2022-04-19 18:52:55 +0900

    first commit

---
+++ posts/models.py
@@ -0,0 +1,20 @@
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    image = models.ImageField(verbose_name='이미지', null=True, blank=True)
+    content = models.TextField(verbose_name='질문')
+    created_at = models.DateTimeField(verbose_name='생성자', auto_now_add = True)
+    
+    view_count = models.IntegerField(verbose_name='조회수', default = 0)
+    writer = models.ForeignKey(to= User, on_delete=models.CASCADE, null=True, blank=True)
+
+class Comment(models.Model):
+    content = models.TextField(verbose_name='답변')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+    post = models.ForeignKey(to ='Post', on_delete=models.CASCADE)
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE)
