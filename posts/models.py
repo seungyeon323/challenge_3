7f703820920a0782544c0bc843dc9f4b6f1c2b62
SEUNGYEON YOO <seungyeon323@naver.com>
2022-04-21 20:08:14 +0900

    third commit

--- posts/models.py
+++ posts/models.py
@@ -17,6 +17,8 @@ class Post(models.Model):
     image = models.CharField(verbose_name='질문제목', max_length=80)
     content = models.TextField(verbose_name='질문')
     category = models.DateTimeField(verbose_name='카테고리', max_length=2, choices=CATEGORY_CHOICE)
+
+
     
     created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
     created_at = models.DateTimeField(verbose_name='최종수정일시', auto_now_add=True)
